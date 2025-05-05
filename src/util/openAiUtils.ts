bd3b0295bee4eb0cfb6985f98650d3e02df2f2e9
Mike A <5902658+Mikodin@users.noreply.github.com>
2025-05-05 11:20:57 -0400

    Adds new models

--- src/util/openAiUtils.ts
+++ src/util/openAiUtils.ts
@@ -16,8 +16,10 @@ import { LanguageOptions } from './consts';
 import { convertToSafeJsonKey } from './textUtil';
 
 export enum LLM_MODELS {
-  'gpt-4o-mini' = 'gpt-4o-mini',
+  'gpt-4.1' = 'gpt-4.1',
+  'gpt-4.1-mini' = 'gpt-4.1-mini',
   'gpt-4o' = 'gpt-4o',
+  'gpt-4o-mini' = 'gpt-4o-mini',
   'gpt-4-turbo' = 'gpt-4-turbo',
 }
 
